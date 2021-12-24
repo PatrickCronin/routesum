4eeae58051b50e0e2eca294eea031398ec140ed2
Patrick Cronin <patrick@gwana.co>
2021-12-24 17:27:37 -0500

    os & io are preferred over io/ioutil since 1.16

--- cmd/routesum/args_test.go
+++ cmd/routesum/args_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"testing"
@@ -18,14 +17,14 @@ func TestArgs(t *testing.T) {
 	assert.Equal(t, "-", args.outputPath, "default outputPath")
 
 	// specified values are respected
-	tempdir, err := ioutil.TempDir("", "routesum-test-")
+	tempdir, err := os.MkdirTemp("", "routesum-test-")
 	require.NoError(t, err, "create temp directory")
 	defer func() {
 		err := os.RemoveAll(tempdir)
 		require.NoError(t, err, "remove temp directory")
 	}()
 	inPath := filepath.Join(tempdir, "in.txt")
-	err = ioutil.WriteFile(inPath, []byte("192.0.2.0\n192.0.2.1\n"), 0o644)
+	err = os.WriteFile(inPath, []byte("192.0.2.0\n192.0.2.1\n"), 0o644)
 	require.NoError(t, err, "write to temp input file")
 
 	args, err = _parseArgs([]string{
