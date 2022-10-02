694639117eebddb7f5e57ead9fc3bf57d1db66ab
Patrick Cronin <patrick@gwana.co>
2022-10-01 20:53:38 -0400

    Replace bitslice with uint128-based route types

--- pkg/routesum/rstrie/rstrie.go
+++ pkg/routesum/rstrie/rstrie.go
@@ -1,92 +1,115 @@
-// Package rstrie provides a datatype that supports building a space-efficient summary of networks and IPs.
+// Package rstrie provides a datatype that supports building a space-efficient summary of networks
+// and IPs.
 package rstrie
 
 import (
-	"bytes"
 	"container/list"
 	"unsafe"
 
-	"github.com/PatrickCronin/routesum/pkg/routesum/bitslice"
+	"github.com/PatrickCronin/routesum/pkg/routesum/routetype"
 )
 
-// RSTrie is a radix-like trie of radix 2 whose stored "words" are the binary representations of networks and IPs. An
-// optimization rstrie makes over a generic radix tree is that since routes covered by other routes don't need to be
-// stored, each node in the trie will have either 0 or 2 children; never 1.
-type RSTrie struct {
-	root *node
+type route[T any] interface {
+	*routetype.V4 | *routetype.V6
+	Bits() uint8
+	CommonAncestor(T) T
+	Contains(T) bool
+	NthBit(uint8) uint8
+	Size() uintptr
 }
 
-type node struct {
-	children *[2]*node
-	bits     bitslice.BitSlice
+type node[T route[T]] struct {
+	route    T
+	children *[2]*node[T]
 }
 
-// NewRSTrie returns an initialized RSTrie for use
-func NewRSTrie() *RSTrie {
-	return &RSTrie{
+// RSTrie is a radix-like trie of radix 2 whose stored "words" are the binary representations of
+// networks and IPs. An optimization rstrie makes over a generic radix tree is that since routes
+// covered by other routes don't need to be stored, each node in the trie will have either 0 or 2
+// children; never 1.
+type RSTrie[T route[T]] struct {
+	root *node[T]
+}
+
+// New returns an initialized RSTrie for use
+func New[T route[T]]() *RSTrie[T] {
+	return &RSTrie[T]{
 		root: nil,
 	}
 }
 
-// InsertRoute inserts a new BitSlice into the trie. Each insert results in a space-optimized trie structure
-// representing its contents. If a route being inserted is already covered by an existing route, it's simply ignored. If
-// a route being inserted covers one or more routes already in the trie, those nodes are removed and replaced by the new
-// route.
-func (t *RSTrie) InsertRoute(routeBits bitslice.BitSlice) {
+// InsertRoute inserts a new Route into the trie. Each insert results in a space-optimized trie
+// structure representing its contents. If a route being inserted is already covered by an existing
+// route, it's simply ignored. If a route being inserted covers one or more routes already in the
+// trie, those nodes are removed and replaced by the new route.
+func (t *RSTrie[T]) InsertRoute(r T) { //nolint: funlen
 	// If the trie has no root node, simply create one to store the new route
 	if t.root == nil {
-		t.root = &node{
-			bits:     routeBits,
+		t.root = &node[T]{
+			route:    r,
 			children: nil,
 		}
 		return
 	}
 
-	// Otherwise, perform a non-recursive search of the trie's nodes for the best place to insert the route, and do so.
-	visited := []*node{}
+	// Otherwise, perform a non-recursive search of the trie's nodes for the best place to insert
+	// the route, and do so.
+	visited := []*node[T]{}
+	var traversedChild uint8
 	curNode := t.root
-	remainingRouteBits := routeBits
 
 	for {
-		remainingRouteBitsLen := len(remainingRouteBits)
-		curNodeBitsLen := len(curNode.bits)
-
-		// Does the requested route cover the current node? If so, update the current node.
-		if remainingRouteBitsLen <= curNodeBitsLen && bytes.HasPrefix(curNode.bits, remainingRouteBits) {
-			curNode.bits = remainingRouteBits
+		// Does the requested prefix contain the current node's prefix? If so, update the current
+		// node.
+		if r.Contains(curNode.route) {
+			curNode.route = r
 			curNode.children = nil
-			return
 		}
 
-		if curNodeBitsLen <= remainingRouteBitsLen && bytes.HasPrefix(remainingRouteBits, curNode.bits) {
+		if curNode.route.Contains(r) {
 			// Does the current node cover the requested route? If so, we're done.
 			if curNode.isLeaf() {
 				return
 			}
 
 			// Otherwise, we traverse to the correct child.
-			remainingRouteBits = remainingRouteBits[curNodeBitsLen:]
 			visited = append(visited, curNode)
-			curNode = curNode.children[remainingRouteBits[0]]
+			traversedChild = r.NthBit(curNode.route.Bits() + 1)
+			curNode = curNode.children[traversedChild]
+
 			continue
 		}
 
-		// Otherwise the requested route diverges from the current node. We'll need to split the current node.
+		// Otherwise the requested route diverges from the current node. We'll need to split the
+		// current node.
 
-		// As an optimization, if the split would result in a new node whose children represent a complete subtrie, we
-		// just update the current node, instead of allocating new nodes and optimizing them away immediately after.
+		// As an optimization, if the split would result in a new node whose children represent a
+		// complete subtrie, we just update the current node, instead of allocating new nodes and
+		// optimizing them away immediately after.
+		commonRouteAncestor := curNode.route.CommonAncestor(r)
 		if curNode.isLeaf() &&
-			curNodeBitsLen == remainingRouteBitsLen &&
-			commonPrefixLen(curNode.bits, remainingRouteBits) == len(curNode.bits)-1 {
-			curNode.bits = curNode.bits[:len(curNode.bits)-1]
-			curNode.children = nil
+			curNode.route.Bits() == r.Bits() &&
+			commonRouteAncestor.Bits() == curNode.route.Bits()-1 {
+			curNode.route = commonRouteAncestor
 		} else {
-			newNode := splitNodeForRoute(curNode, remainingRouteBits)
+			var lowerChild, upperChild *node[T]
+			if r.NthBit(commonRouteAncestor.Bits()+1) == 0 {
+				lowerChild = &node[T]{route: r}
+				upperChild = &node[T]{route: curNode.route, children: curNode.children}
+			} else {
+				lowerChild = &node[T]{route: curNode.route, children: curNode.children}
+				upperChild = &node[T]{route: r}
+			}
+			newNode := &node[T]{
+				route:    commonRouteAncestor,
+				children: &[2]*node[T]{lowerChild, upperChild},
+			}
+
 			visitedLen := len(visited)
 			if visitedLen == 0 {
 				t.root = newNode
 			} else {
-				visited[visitedLen-1].children[newNode.bits[0]] = newNode
+				visited[visitedLen-1].children[traversedChild] = newNode
 			}
 		}
 
@@ -95,7 +118,11 @@ func (t *RSTrie) InsertRoute(routeBits bitslice.BitSlice) {
 	}
 }
 
-func (n *node) childrenAreCompleteSubtrie() bool {
+func (n *node[T]) isLeaf() bool {
+	return n.children == nil
+}
+
+func (n *node[T]) childrenAreCompleteSubtrie() bool {
 	if n.isLeaf() {
 		return false
 	}
@@ -104,43 +131,22 @@ func (n *node) childrenAreCompleteSubtrie() bool {
 		return false
 	}
 
-	if len(n.children[0].bits) != 1 || len(n.children[1].bits) != 1 {
+	if n.children[0].route.Bits() != n.children[1].route.Bits() ||
+		n.children[0].route.Bits() != n.route.Bits()+1 {
 		return false
 	}
 
 	return true
 }
 
-func (n *node) isLeaf() bool {
-	return n.children == nil
-}
-
-func splitNodeForRoute(oldNode *node, routeBits bitslice.BitSlice) *node {
-	commonBitsLen := commonPrefixLen(oldNode.bits, routeBits)
-	commonBits := oldNode.bits[:commonBitsLen]
-
-	routeNode := &node{
-		bits:     routeBits[commonBitsLen:],
-		children: nil,
-	}
-	oldNode.bits = oldNode.bits[commonBitsLen:]
-
-	newNode := &node{
-		bits:     commonBits,
-		children: &[2]*node{},
-	}
-	newNode.children[routeNode.bits[0]] = routeNode
-	newNode.children[oldNode.bits[0]] = oldNode
-
-	return newNode
-}
-
-// A completed subtrie is a node in the trie whose children when taken together represent the complete subtrie below the
-// node. For example, if a node represented the route "00", and it had a child for "0" and a child for "1", the node
-// would be representing the "000" and "001" routes. But that's the same as having a single node for "00".
-// simplifyCompletedSubtries takes a stack of visited nodes and simplifies completed subtries as far down the stack as
-// possible. If at any point in the stack we find a node representing an incomplete subtrie, we stop.
-func simplifyVisitedSubtries(visited []*node) {
+// A completed subtrie is a node in the trie whose children when taken together represent the
+// complete subtrie below the node. For example, if a node represented the route "00", and it had a
+// child for "0" and a child for "1", the node would be representing the "000" and "001" routes. But
+// that's the same as having a single node for "00".
+// simplifyCompletedSubtries takes a stack of visited nodes and simplifies completed subtries as far
+// down the stack as possible. If at any point in the stack we find a node representing an
+// incomplete subtrie, we stop.
+func simplifyVisitedSubtries[T route[T]](visited []*node[T]) {
 	for i := len(visited) - 1; i >= 0; i-- {
 		if visited[i].isLeaf() {
 			return
@@ -154,71 +160,7 @@ func simplifyVisitedSubtries(visited []*node) {
 	}
 }
 
-func commonPrefixLen(a, b bitslice.BitSlice) int {
-	i := 0
-	maxLen := min(len(a), len(b))
-	for ; i < maxLen; i++ {
-		if a[i] != b[i] {
-			break
-		}
-	}
-
-	return i
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-
-	return b
-}
-
-type traversalStep struct {
-	n                  *node
-	precedingRouteBits bitslice.BitSlice
-}
-
-// Contents returns the BitSlices contained in the RSTrie.
-func (t *RSTrie) Contents() []bitslice.BitSlice {
-	// If the trie is empty
-	if t.root == nil {
-		return []bitslice.BitSlice{}
-	}
-
-	// Otherwise
-	remainingSteps := list.New()
-	remainingSteps.PushFront(traversalStep{
-		n:                  t.root,
-		precedingRouteBits: bitslice.BitSlice{},
-	})
-
-	contents := []bitslice.BitSlice{}
-	for remainingSteps.Len() > 0 {
-		step := remainingSteps.Remove(remainingSteps.Front()).(traversalStep)
-
-		stepRouteBits := bitslice.BitSlice{}
-		stepRouteBits = append(stepRouteBits, step.precedingRouteBits...)
-		stepRouteBits = append(stepRouteBits, step.n.bits...)
-
-		if step.n.isLeaf() {
-			contents = append(contents, stepRouteBits)
-		} else {
-			remainingSteps.PushFront(traversalStep{
-				n:                  step.n.children[1],
-				precedingRouteBits: stepRouteBits,
-			})
-			remainingSteps.PushFront(traversalStep{
-				n:                  step.n.children[0],
-				precedingRouteBits: stepRouteBits,
-			})
-		}
-	}
-
-	return contents
-}
-
-func (t *RSTrie) visitAll(cb func(*node)) {
+func (t *RSTrie[T]) visitAll(cb func(*node[T])) {
 	// If the trie is empty
 	if t.root == nil {
 		return
@@ -226,52 +168,53 @@ func (t *RSTrie) visitAll(cb func(*node)) {
 
 	// Otherwise
 	remainingSteps := list.New()
-	remainingSteps.PushFront(traversalStep{
-		n:                  t.root,
-		precedingRouteBits: bitslice.BitSlice{},
-	})
+	remainingSteps.PushFront(t.root)
 
 	for remainingSteps.Len() > 0 {
-		curNode := remainingSteps.Remove(remainingSteps.Front()).(traversalStep)
+		curNode := remainingSteps.Remove(remainingSteps.Front()).(*node[T])
 
 		// Act on this node
-		cb(curNode.n)
+		cb(curNode)
 
 		// Traverse the remainder of the nodes
-		if !curNode.n.isLeaf() {
-			curNodeRouteBits := bitslice.BitSlice{}
-			curNodeRouteBits = append(curNodeRouteBits, curNode.precedingRouteBits...)
-			curNodeRouteBits = append(curNodeRouteBits, curNode.n.bits...)
-
-			remainingSteps.PushFront(traversalStep{
-				n:                  curNode.n.children[1],
-				precedingRouteBits: curNodeRouteBits,
-			})
-			remainingSteps.PushFront(traversalStep{
-				n:                  curNode.n.children[0],
-				precedingRouteBits: curNodeRouteBits,
-			})
+		if !curNode.isLeaf() {
+			remainingSteps.PushFront(curNode.children[1])
+			remainingSteps.PushFront(curNode.children[0])
 		}
 	}
 }
 
+// Contents returns the routes contained in the RSTrie.
+func (t *RSTrie[T]) Contents() []T {
+	var contents []T
+
+	t.visitAll(func(n *node[T]) {
+		if n.isLeaf() {
+			contents = append(contents, n.route)
+		}
+	})
+
+	return contents
+}
+
 // MemUsage returns information about an RSTrie's current size in memory.
-func (t *RSTrie) MemUsage() (uint, uint, uintptr, uintptr) {
+func (t *RSTrie[T]) MemUsage() (uint, uint, uintptr, uintptr) {
 	var numInternalNodes, numLeafNodes uint
 	var internalNodesTotalSize, leafNodesTotalSize uintptr
 
-	tallyNode := func(n *node) {
-		baseNodeSize := unsafe.Sizeof(node{}) + uintptr(cap(n.bits))*unsafe.Sizeof([1]byte{}) //nolint: exhaustruct, gosec, lll
+	baseNodeSize := unsafe.Sizeof(node[T]{}) //nolint:  gosec
+
+	t.visitAll(func(n *node[T]) {
+		nodeSize := baseNodeSize + n.route.Size()
 		if n.isLeaf() {
 			numLeafNodes++
-			leafNodesTotalSize += baseNodeSize
+			leafNodesTotalSize += nodeSize
 			return
 		}
 
 		numInternalNodes++
-		internalNodesTotalSize += baseNodeSize + unsafe.Sizeof([2]*node{}) //nolint: gosec
-	}
-	t.visitAll(tallyNode)
+		internalNodesTotalSize += nodeSize + unsafe.Sizeof([2]node[T]{}) //nolint: gosec
+	})
 
 	return numInternalNodes, numLeafNodes, internalNodesTotalSize, leafNodesTotalSize
 }
