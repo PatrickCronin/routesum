369b2708c6965b906ade8f531a03b5cf157039f8
Patrick Cronin <patrick@gwana.co>
2022-10-06 09:04:38 -0400

    Facilitate CPU profiling in the feature branch

--- cmd/routesum/main.go
+++ cmd/routesum/main.go
@@ -4,21 +4,46 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"flag"
 	"fmt"
 	"io"
 	"os"
+	"runtime/pprof"
 
 	"github.com/PatrickCronin/routesum/pkg/routesum"
+	"github.com/pkg/errors"
 )
 
 func main() {
-	if err := summarize(os.Stdin, os.Stdout); err != nil {
+	cpuprofile := flag.String("cpuprofile", "", "write cpu profile to file")
+	flag.Parse()
+
+	var cpuProfOut io.Writer
+	if *cpuprofile != "" {
+		var err error
+		if cpuProfOut, err = os.Create(*cpuprofile); err != nil {
+			fmt.Fprint(os.Stderr, errors.Wrap(err, "create cpu profile output file").Error())
+			os.Exit(1)
+		}
+	}
+
+	if err := summarize(os.Stdin, os.Stdout, cpuProfOut); err != nil {
 		fmt.Fprintf(os.Stderr, "summarize: %s\n", err.Error())
 		os.Exit(1)
 	}
 }
 
-func summarize(in io.Reader, out io.Writer) error {
+func summarize(
+	in io.Reader,
+	out, cpuProfOut io.Writer,
+) error {
+	if cpuProfOut != nil {
+		if err := pprof.StartCPUProfile(cpuProfOut); err != nil {
+			return errors.Wrap(err, "start cpu profiling")
+		}
+		defer pprof.StopCPUProfile()
+	}
+
 	rs := routesum.NewRouteSum()
 	scanner := bufio.NewScanner(in)
 	for scanner.Scan() {
